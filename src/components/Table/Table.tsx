2981a35dd1004193752a051000e89f6a3497741a
Tristan Samuk <tsamuk@gmail.com>
2024-06-11 09:47:03 -0400

    Refactor table header for better dynamic rendering

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -3,14 +3,23 @@ import TableHeader from "../TableHeader/TableHeader";
 import tableData from "../../data/table-data.json";
 import "./Table.scss";
 
+const tableHeadings = [
+  "Coin",
+  "24hr change",
+  "Live price",
+  "Sell price",
+  "Buy price",
+  "Watch",
+];
+
 const Table = () => {
   return (
-    <div className="table">
-      <TableHeader />
+    <table className="table">
+      <TableHeader tableHeadings={tableHeadings} />
       {/* {tableData.map((coin) => {
         return <TableRow />;
       })} */}
-    </div>
+    </table>
   );
 };
 
